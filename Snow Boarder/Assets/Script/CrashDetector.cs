76945cb1ec86d0cb12332657ecd741e803ffc0e2
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2023-10-23 21:51:16 +0700

    base setup

--- Snow Boarder/Assets/Script/CrashDetector.cs
+++ Snow Boarder/Assets/Script/CrashDetector.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-  
+    [SerializeField] float reloadTime = 0.5f;
    void OnTriggerEnter2D(Collider2D other)
    {
        if (other.tag == "Ground"){
-
+             Invoke("ReloadScene",reloadTime);
        }
+   }
+
+
+   void ReloadScene(){
+        SceneManager.LoadScene(0);
    }
 }
--- Snow Boarder/Assets/Script/FinishLine.cs
+++ Snow Boarder/Assets/Script/FinishLine.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float reloadTime = 1f;
+   void OnTriggerEnter2D(Collider2D other)
+   {
+       if (other.tag == "Player"){
+            Invoke("ReloadScene",reloadTime);
+       }
+   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void ReloadScene(){
+        SceneManager.LoadScene(0);
+   }
 }
